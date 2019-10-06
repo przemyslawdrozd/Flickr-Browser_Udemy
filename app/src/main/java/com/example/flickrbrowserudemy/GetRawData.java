a6e784eb07602c82208f484496189c48cb5f679d
przemo06133 <przemo06133@gmail.com>
2019-10-06 12:51:08 +0200

    Delete logger with data

--- app/src/main/java/com/example/flickrbrowserudemy/GetRawData.java
+++ app/src/main/java/com/example/flickrbrowserudemy/GetRawData.java
@@ -29,9 +29,20 @@ class GetRawData extends AsyncTask<String, Void, String> {
         mCallback = callback;
     }
 
+    void runInSameThread(String s) {
+        Log.d(TAG, "runInSameThread: starts");
+//        onPostExecute(doInBackground(s));
+        if (mCallback != null) {
+            /**String result = doInBackground(s);
+            mCallback.onDownloadComplete(result, mDownloadStatus);*/
+            mCallback.onDownloadComplete(doInBackground(s), mDownloadStatus);
+        }
+        Log.d(TAG, "runInSameThread: ends");
+    }
+
     @Override
     protected void onPostExecute(String s) {
-        Log.d(TAG, "onPostExecute: parameter = " + s.length());
+//        Log.d(TAG, "onPostExecute: parameter = " + s.length());
         if (mCallback != null) {
             mCallback.onDownloadComplete(s, mDownloadStatus);
         }
